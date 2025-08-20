4e664dba2c00b3e6be375a535def98429d1d8937
DigitalRage <blaine.buckler@ucas-edu.net>
2025-08-20 09:52:59 -0600

    work

---
+++ notes/inputs.py
@@ -0,0 +1,19 @@
+# Blaine Buckler 1st Inputs and Outputs Notes
+
+#5 What is an input
+#A way for the code to get information
+
+#6 what is the ourpose of inputs
+#Code isn't helpful if it doesn't use new information
+name = input("What is your name: ")
+#  ^      ^           ^
+#Save the input where it can be accesed again
+#Use keyword "input"
+#inside parens give user instructions
+print("Hello", name + ".")
+print("what is your name: ")
+name_two = input
+#both instruction methods are fine
+num_one = input("Type a number:")
+num_two = input()
+print(num_one + num_two)
