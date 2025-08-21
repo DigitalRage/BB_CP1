72481e3f488c0b33992be8fe4a3de5547de7224d
DigitalRage <blaine.buckler@ucas-edu.net>
2025-08-21 09:52:14 -0600

    Variable Notes

---
+++ starters/seasons.py
@@ -0,0 +1,5 @@
+# Blaine Buckler 1st Seasons Practice
+lName = input("What is your last name: ")
+print("What is your favorite season", lName + ": ", end="")
+season = input()
+print("Wow", lName, "san, I like", season, "too!")
