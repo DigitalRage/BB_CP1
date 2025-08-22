309559561bc3799b1d9945bb1c38c15942ce884d
DigitalRage <blaine.buckler@ucas-edu.net>
2025-08-22 16:32:36 +0000

    Finished Unit Project

---
+++ unit_projects/tempCodeRunnerFile.py
@@ -0,0 +1,8 @@
+    
+        color = input("What is your favorite color: ")
+        colors.append(color)
+        
+        age = input("Please enter your age: ")
+        ages.append(age)
+            
+        prin
--- unit_projects/who_are_you.py
+++ unit_projects/who_are_you.py
@@ -1,20 +1,23 @@
 # Blaine Buckler 1st Who Are You? 
 i = 0
-while True: 
-    names = []
-    for i in range(1): 
-        name = input("What is your name: ")
-        names.append(name)
+names = []
+colors = []
+ages = []
 
-        colors = []
-    for i in range(1): 
+while True: 
+    name = input("What is your name: ")
+    
+    if name in names: 
+        position = names.index(name)
+        
+        print(f"Hello {name}. I see you like {colors[position]}, INTERESTING... And your age is {ages[position]}. I love {ages[position]} year olds. ")
+    else:
+        names.append(name)
+        
         color = input("What is your favorite color: ")
         colors.append(color)
-
-    ages = []
-    for i in range(1): 
+        
         age = input("Please enter your age: ")
         ages.append(age)
-        
-    for name in names: 
-        print("Hello", name + ". I see you like", color + ", INTERESTING...", "And your age is", age + ". I love", age, "year olds. ")
+            
+        print(f"Hello {name}. I see you like {color}, INTERESTING... And your age is {age}. I love {age} year olds. ")
