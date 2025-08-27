8f0f444f16e65c9b78e17914c96fe3b047a56dcd
DigitalRage <blaine.buckler@ucas-edu.net>
2025-08-27 09:52:41 -0600

    Work

---
+++ notes/numbers.py
@@ -0,0 +1,41 @@
+# Blaine Buckler 1st Integers and Floats Notes
+
+
+#Intergers
+age = 99999999999999999999
+year = 2015
+hour = 9
+motivation = -5
+
+#Float (Floating Point Number)
+
+pi = 3.14
+percent = .75
+damage = 9.75
+
+#MATH
+print(5.0+2) #7
+print(5-2) #3
+print(5*2) #10
+print(5/2) #2.5
+print(5//2) #2
+print(5%2) #1
+print(5**2) #25
+
+print(5+2 *9 + (5**5)%3)
+
+#type casting
+num = int(3.14)
+decimal = float(8)
+print(num)
+print(decimal)
+
+#Using Variables
+hours = 3
+minutes = 4
+
+#To change a user input data type you have to surround the entire input. 
+print(hours*60+minutes)
+
+age = int(input("How old are you: \n"))
+print("Wow you are old that is", 5256*age, "minutes!")
