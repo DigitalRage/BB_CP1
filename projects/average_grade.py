860c5f49afa2a638735cc16235c141d6b10bc3e3
DigitalRage <blaine.buckler@ucas-edu.net>
2025-08-28 16:11:20 +0000

    Work

--- projects/average_grade.py
+++ projects/average_grade.py
@@ -1,8 +1,12 @@
 # Blaine Buckler 1st Average Grade Project
-class_num=1
+class_num = 0
 grades=[]
 range(len(grades))
-class=["first","second","third","fourth","fith","sixth","seventh"]
-for grade in grades: 
-    input(f"What is your {class} class grade: ")
-    class.index()
+class_names = ["first", "second", "third", "fourth", "fifth", "sixth", "seventh"]
+while class_num < len(class_names):
+    grade = float(input(f"What is your {class_names[class_num]} class grade: "))
+    grades.append(grade)
+    class_num += 1 
+
+average = sum(grades)/len(grades)
+print(f"Your current grade average is {average:.2f}")
