6cc54651bcda8165c8194a1b139f05a63773aaa6
DigitalRage <blaine.buckler@ucas-edu.net>
2025-09-08 09:53:09 -0600

    Work

---
+++ projects/basic_calculator.py
@@ -0,0 +1,10 @@
+# BB 1st Basic Calculator Project
+
+num1 = float(input("Please type number: "))
+num2 = float(input("Please type another number: "))
+
+add = num1 + num2
+subtract = num1 - num2
+multiply = num1 * num2
+divide = num1 / num2
+idivide = num1 // num2
