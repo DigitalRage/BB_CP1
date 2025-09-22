f8c2d190ded80af48a6fb8a43ccbd91a7d3b5a46
DigitalRage <blaine.buckler@ucas-edu.net>
2025-09-22 16:25:51 +0000

    Finnish User SignIn

--- projects/user_signin.py
+++ projects/user_signin.py
@@ -1,33 +1,25 @@
 # BB 1st User Sign In Project
 
 names = []
-paswords = []
+passwords = []
 
 
 while True: 
-    name = input("What is your username?\n").strip
-    password = input("What is your Password, Gime it!\n").strip
+    name = input("What is your username?\n").strip()
+    password = input("What is your Password, Gime it!\n").strip()
 
-    if name in names and name == True: 
+    if name in names: 
         position = names.index(name)
-        print(f"Hello {name[position]}. Your username and password looks right.")
-    elif name == True: 
-        if password == True: 
-            print("Yo, you have a valid username andpassword!!!")
-        else:
-            print("Type something for your pasword you dingus")
+        if passwords[position] == password: 
+            print(f"Hello {names[position]}. Your username and password looks right.")
+        else: 
+            print("Wrong password try again. ")
+    elif name and password: 
+        names.append(name)
+        passwords.append(password)
+        print("Yo, you have a valid username and password!!!")
+    elif not password:
+        print("Type something for your pasword you dingus")
     else: 
         print("JUST TYPE SOMETHING!!!")
-    
-    
         
-"""    else:
-        names.append(name)
-        
-        color = input("What is your favorite color: ")
-        colors.append(color)
-        
-        age = input("Please enter your age: ")
-        ages.append(age)
-            
-        print(f"Hello {name}. I see you like {color}, INTERESTING... And your age is {age}. I love {age} year olds. ")"""
