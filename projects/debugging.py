a31f50d634ff0ea1f2714eb4d9bd83a4ea6e6bc7
DigitalRage <blaine.buckler@ucas-edu.net>
2025-09-25 09:53:18 -0600

    Suff

---
+++ projects/debugging.py
@@ -0,0 +1,15 @@
+# BB 1st Debugging Notes 
+
+# Syntax Error - grammar error
+# print"Vienna"
+# => Read error message, go to that line of code, fix the syntax
+
+# Logic Errors - We gave the wrong steps
+numone = "2"
+numtwo = "5"
+
+print(numone+numtwo)
+
+# Have a well thought out plan, Step by step go through our logic 
+
+# Run-time Error
