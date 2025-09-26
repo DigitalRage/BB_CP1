bf18f88728a13a44e9bf156d7446e195d333c0ef
DigitalRage <blaine.buckler@ucas-edu.net>
2025-09-26 09:40:32 -0600

    debug

--- projects/debugging.py
+++ projects/debugging.py
@@ -12,4 +12,12 @@ print(numone+numtwo)
 
 # Have a well thought out plan, Step by step go through our logic 
 
-# Run-time Error
+# Run-time Error - it ends with num/0
+import random
+import time
+
+while True:
+    time.sleep(0.05)
+    denominator = random.randint(1,5)
+
+    print(10/denominator)
