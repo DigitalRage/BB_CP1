02bfb674001a69c3139860805484c8cf16989391
DigitalRage <blaine.buckler@ucas-edu.net>
2025-10-02 01:18:01 +0000

    Work

--- projects/list_manager.py
+++ projects/list_manager.py
@@ -1,29 +1,35 @@
-# BB 1st Shopping List Manager
-print("This is a Shopping List Manager, use it as you please. ")
+# BB 1st Shopping list Manager
+print("This is a Shopping list Manager, use it as you please. ")
 
-list = []
+s_list = []
 
 while True: 
-    action = input("What do you want to do with the item?\n   1. Add to list\n   2. Remove item in list\n   3. Show List\n   4. Quit\n   5. Mark Done\n ")
+    action = input("What do you want to do with the item?\n   1. Add to list\n   2. Remove item in list\n   3. Show list\n   4. Quit\n   5. Mark Done\n ")
 
     if action == "1":
-        item = input("What is the name of your item? \n").strip()
-        list.append(item + "\n")
+        item = input("What is the name of your item? \n").strip().lower().title()
+        s_list.append(item)
+        print(f"{item} is added to the list. ")
     elif action == "2":
-        item = input("What is the name of your item? \n").strip()
-        if item in list: 
-           list.remove(item)
+        item = input("What is the name of your item? \n").strip().title()
+        if item in s_list: 
+           s_list.remove(item)
         else: 
            print("Try again")
     elif action == "3": 
-        print(*list)
+        print(*s_list)
     elif action == "4": 
         print("So long cruel world!")
         break
     elif action == "5": 
-        item = input("What is the name of your item? \n").strip()
-        if item in list: 
-            list[item] += " done"
+        item = input("What is the name of your item? \n").strip().title()
+        if item in s_list: 
+            i = s_list.index(item)
+            if " done" not in s_list[i]: 
+                s_list[i] = item + " done"
+                print('The deed is "done"')
+            else:
+                print("Item is already marked as done.")
         else: 
             print("Try Again")
     elif action == "Cave Story": 
