ff1c9ebfe65308a65cb73d20e91c847687961d4e
DigitalRage <blaine.buckler@ucas-edu.net>
2025-10-13 15:06:10 +0000

    work

--- projects/password_strength.py
+++ projects/password_strength.py
@@ -1,27 +1,65 @@
 #BB 1st Password Strength Project
 import re#https://www.geeksforgeeks.org/python/python-program-to-check-if-a-string-has-at-least-one-letter-and-one-number/
 
-p_word = input("What is your password?\n").strip()#This is to get the password
+pwords = []
 
-strength = 5 #Holds the number for how string the password is
+while True:
+    p_word = input("What is your password?\n").strip()#This is to get the password
 
-lowercase = bool(re.search(r'[a-z]', p_word))
-uppercase = bool(re.search(r'[A-Z]', p_word))
+    pwords.append(p_word)
 
+    strength = 5 #Holds the number for how string the password is
 
-print(lowercase)
-print(uppercase)
+    lowercase = bool(re.search(r'[a-z]', p_word))#Used to check if lowercase letters are there
+    uppercase = bool(re.search(r'[A-Z]', p_word))#Used to check if uppercase letters are there
+    numbers = bool(re.search(r'[0-9]', p_word))#Used to check if numbers are there
+    special_char = bool(re.search(r'[!@#$%^&*()_+\-=\[\]{};\':"\\|,.<>\/?~`]', p_word))#Used to check if special characters are there
 
-numbers = [1,2,3,4,5,6,7,8,9,0]
-letters = ['q','w','e','r','t','y','u','i',]
+    #print(lowercase)
+    #print(uppercase)
+    #print(numbers)
+    #print(special_char)
+    #Previous lines are used to check if the variables are true or false
 
-if len(p_word) < 8: 
-    strength -= 1
-else: 
-        pass
+    if len(p_word) < 8: 
+        strength -= 1
+        print("Password Length Unsufficent\n")
+    else: 
+        print("Password Length Sufficent\n")
 
-if uppercase == False: 
-    strength -=1
-else: 
-     pass
+    if uppercase == False: 
+        strength -=1
+        print("Password UpperCase Unsufficent\n")
+    else: 
+        print("Password UpperCase Sufficent\n")
 
+    if lowercase == False: 
+        strength -=1
+        print("Password LowerCase Unsufficent\n")
+    else: 
+        print("Password LowerCase Sufficent\n")
+
+    if numbers == False: 
+        strength -=1
+        print("Password Digits Unsufficent\n")
+    else: 
+        print("Password Digits Sufficent\n")
+
+    if special_char == False: 
+        strength -=1
+        print("Password Special Characters Unsufficent\n")
+    else: 
+        print("Password Special Characters Sufficent\n")
+
+    if strength == 5: 
+        print("🟩🟩🟩🟩🟩100%\nVery Strong")
+    elif strength == 4: 
+        print("🟩🟩🟩🟩🟥80%\nStrong")
+    elif strength == 3: 
+        print("🟩🟩🟩🟥🟥60%\nModerate")
+    elif strength == 2: 
+        print("🟩🟩🟥🟥🟥40%\nWeak")
+    elif strength == 1: 
+        print("🟩🟥🟥🟥🟥20%\nWeak")
+    elif strength == 0: 
+        print("🟥🟥🟥🟥🟥0%\nVery Weak")
