b49e3509a8341d840d27633723753c3328ef5272
DigitalRage <blaine.buckler@ucas-edu.net>
2025-10-22 16:30:01 +0000

    Work

--- projects/caesar_cipher.py
+++ projects/caesar_cipher.py
@@ -5,7 +5,7 @@ print("This is a Caeser Ciper Encoder/Decoder. \nWhat do You want to do? ")
 def encoder(encoded_message):
     encoded_message = list(encode_message)
     encoded_message = ord(encoded_message)
-    encoded_message = encoded_message += 
+    encoded_message = encoded_message += number
 
     return encoded_message
 #main loop
@@ -22,6 +22,18 @@ while True:
         continue
     #if you are encoding, encode the message
     if decision == 1: 
+        #user encode message
         encode_message = input("Type the message you want to encode: \n")
+        #what number they want to encode it to
+        number = input("What number do you want to encode to?\n")
+        #checks if the number is a number
+        if number.isdigit():
+            #makes the number a number it is a number
+            number = int(number)
+        else:
+            #makes you redo if it is false
+            print("Try Again")
+            continue
+
         encoded_message = encoder
     elif decision == 2: 
