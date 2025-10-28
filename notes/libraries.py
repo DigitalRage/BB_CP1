137a6ab9f57833a13abac12b33949f02dc375046
DigitalRage <blaine.buckler@ucas-edu.net>
2025-10-28 09:52:07 -0600

    stuff

---
+++ notes/libraries.py
@@ -0,0 +1,51 @@
+#BB 1st Libraries and built in functions
+import turtle as t
+import random
+colors = ["red", "blue", "green", "yellow", "orange", "purple", "pink", "brown"]
+side = random.randint(10, 500)
+
+
+
+t.speed(0)
+
+
+t.color(random.choice(colors))
+t.shape("turtle")
+
+t.width(50)
+t.fillcolor(random.choice(colors))
+t.begin_fill()
+t.forward(250)
+t.right(90)
+t.forward(250)
+t.right(90)
+t.forward(250)
+t.right(90)
+t.forward(250)
+t.right(90)
+t.end_fill()
+
+t.penup()
+t.goto(50, 50)
+
+t.pendown()
+t.fillcolor(random.choice(colors))
+t.begin_fill()
+t.forward(250)
+t.right(90)
+t.forward(250)
+t.right(90)
+t.forward(250)
+t.right(90)
+t.forward(250)
+t.right(90)
+t.end_fill()
+
+for i in range(1, 1000):
+    t.forward(1+i)
+    t.right(120)
+    t.forward(20)
+    t.color(random.choice(colors))
+
+t.done()
+
