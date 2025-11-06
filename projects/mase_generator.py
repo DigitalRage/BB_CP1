bb4eff6b4eab625c07c8b686d416f5a139b94aa8
DigitalRage <blaine.buckler@ucas-edu.net>
2025-11-06 09:50:25 -0700

    Stuff

--- projects/mase_generator.py
+++ projects/mase_generator.py
@@ -30,41 +30,61 @@ t.right(180)
 
 for i in range(1,5000):
     
-    x = r.randint(1,9)
-    y = r.randint(1,9)
+    x1 = r.randint(1,9)
+    y1 = r.randint(1,9)
 
     #checks what angle the line is, so that it can go that way
     rotation = r.randint(1,4)
     angle = rotation * 90
     if rotation == 1: 
-        x2 = x
-        y2 = y - 1
+        x2 = x1
+        y2 = y1 - 1
     elif rotation == 2: 
-        x2 = x - 1
-        y2 = y
+        x2 = x1 - 1
+        y2 = y1
     elif rotation == 3: 
-        x2 = x
-        y2 = y + 1
+        x2 = x1
+        y2 = y1 + 1
     elif rotation == 4: 
-        x2 = x + 1
-        y2 = y
+        x2 = x1 + 1
+        y2 = y1
     #Finds the cordinates in the x and y values
     def cord(var): 
         var = var * 50
         return var
 
 
-    x = cord(x)-500
+    x1 = cord(x1)-500
     x2 = cord(x2)-500
-    y = cord(y)
+    y1 = cord(y1)
     y2 = cord(y2)
 
     #x cordinate
     #y cordinite
-    cords.append([x,x2,y,y2,angle])
+    cords.append([x1,x2,y1,y2,angle])
+
+def filtered_cords(data): 
+    filtered_list = []
+    seen_points = set()
+
+    for item in data: 
+        x1, x2, y1, y2, *rest = item
+
+        point1 = (x1,y1)
+        point2 = (x2,y2)
+
+
+    if point1 not in seen_points or point2 not in seen_points: 
+        filtered_list.append(item)
+        seen_points.add(point1)
+        seen_points.add(point2)
+
+    return filtered_list
+
+coords = filtered_cords(cords)
 
 #pulls angle & cordnate
-for list in cords: 
+for list in coords: 
     x = list[0]
     x2 = list[1]
     y = list[2]
