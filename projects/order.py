e3d2fdfa5e7c409c7329feaa94a09d2cd3c96971
DigitalRage <blaine.buckler@ucas-edu.net>
2025-11-11 09:53:07 -0700

    Yeas

---
+++ projects/order.py
@@ -0,0 +1,22 @@
+# BB 1st Order Up! Project
+stoof = {
+    "meel": {
+        "Booger":{
+            "HoomBooger": 10.00, 
+            "ChezBooger": 10.00, 
+            "BagMocWooper": 10.00, 
+            "Dabble Dabble Chris Pratt Style": 10.00
+        }, 
+
+    }, 
+    "Moc aund Chez": {
+        "Tai-Knees Moc aund Chez": 10.00, 
+        "Moxeekhan Moc aund Chez": 10.00, 
+        "Ahmareokhan Moc aund Chez": 1000.00
+    }, 
+    "Tahkoos": {
+        "Sau-pht Tahkoos": 0.01, 
+        "Stwheat Tahkoos": 10.00, 
+        "Delt Tahkoos": 1000.00
+    }
+}
