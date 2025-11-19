81271ebda71e366d4e6e5e3d7f8ccf01eac18aca
DigitalRage <blaine.buckler@ucas-edu.net>
2025-11-19 17:27:09 +0000

    Yes!!!

--- projects/squares.py
+++ projects/squares.py
@@ -1,2 +1,8 @@
 #BB 1st Square Numbers Project
-n=range(20);print(*n);print(*[i*i for i in n])
+#for i in range(1,20):print(f'Original:{i},Squared:{i*i}') #57 Characters Golfed
+numbers = range(1,20)
+squares = map(lambda x: x*x, range(1,20))
+for square in squares:
+    for number in numbers:
+        if square == number * number:
+            print(f'Original:{number},Squared:{square}') #203 Characters UnGolfed
