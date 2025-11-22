1f9a0c42b8ec1f8d9913e7dd1d3032152d17239b
DigitalRage <bliane.buckler@ucas-edu.net>
2025-11-21 17:54:03 -0700

    Yes

--- projects/factorial.py
+++ projects/factorial.py
@@ -1,13 +1,16 @@
 #BB 1st Factorial Calculator Pseudo Code Project
 
+#!!! is added before new lines of pseudo code
 #def can_int(user_inp): 
 #    try to change user_inp to int, return true 
 #    except: return False
 #
 #def factor(user_inp): 
-#    for i in range(user_inp,0,-1): 
-#        sum*i
-#        return sum
+#    !!! set result = 1
+#    !!! for i in range(user_inp down to 1): 
+#    !!!     result = result * i
+#    !!! return result
+#
 #print instructions for inputting a number
 #while True: 
 #    user_num = input()
@@ -15,23 +18,33 @@
 #        break from loop
 #    else: 
 #        print that their input was not valid and to try again
-#print(f"original: {user_num} / factorial {factor(user_num)}")
+#
+#!!! print the multiplication steps (e.g. "5 × 4 × 3 × 2 × 1 = 120")
 
-def can_int(user_inp): 
+def can_int(user_inp):
     try:
         int(user_inp)
-        return True 
-    except: return False
+        return True
+    except:
+        return False
 
-def factor(user_inp): 
-    for i in range(user_inp,0,-1): 
-        sum*=i
-        return sum
-print("Type in a whole number")
-while True: 
+def factor(user_inp):
+    result = 1
+    steps = []
+    for i in range(user_inp, 0, -1):
+        result *= i
+        steps.append(str(i))
+    if user_inp == 0:
+        return "0! = 1"
+    return " × ".join(steps) + " = " + str(result)
+
+print("What number do you want the factorial of:")
+while True:
     user_num = input()
-    if can_int(user_num) is True: 
+    if can_int(user_num):
+        user_num = int(user_num)
         break
-    else: 
+    else:
         print("Input was not valid, try again")
-print(f"original: {user_num} / factorial {factor(user_num)}")
+
+print(factor(user_num))
